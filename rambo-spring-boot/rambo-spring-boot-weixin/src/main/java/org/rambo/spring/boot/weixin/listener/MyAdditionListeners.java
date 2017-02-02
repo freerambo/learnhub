a86f863ba2f47b37ec2796d242cff476b8bc8645
zhuyb <zhuyb@ERIANNB119.staff.main.ntu.edu.sg>
2017-02-02 23:05:39 +0800

    add one additional listener for testing

---
+++ rambo-spring-boot/rambo-spring-boot-weixin/src/main/java/org/rambo/spring/boot/weixin/listener/MyAdditionListeners.java
@@ -0,0 +1,30 @@
+package org.rambo.spring.boot.weixin.listener;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.web.context.WebApplicationContext;
+
+import weixin.popular.example.TokenManagerListener;
+
+public class MyAdditionListeners extends SpringBootServletInitializer {
+
+    protected final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        WebApplicationContext rootAppContext = createRootApplicationContext(servletContext);
+        if (rootAppContext != null) {
+            servletContext.addListener(new TokenManagerListener());
+        }
+        else {
+            this.logger.debug("No ContextLoaderListener registered, as "
+                    + "createRootApplicationContext() did not "
+                    + "return an application context");
+        }
+    }
+    
+}
