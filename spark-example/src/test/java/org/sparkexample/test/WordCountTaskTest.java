b0badcdfb086c80f2426affa1a890e32d85734a5
freerambo <asybzhu@gmail.com>
2017-10-31 11:59:16 +0800

    add an examples with apache spark

---
+++ spark-example/src/test/java/org/sparkexample/test/WordCountTaskTest.java
@@ -0,0 +1,21 @@
+package org.sparkexample.test;
+
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.sparkexample.WordCountTask;
+
+import java.net.URISyntaxException;
+
+public class WordCountTaskTest {
+
+	Logger logger = LoggerFactory.getLogger(WordCountTaskTest.class);
+
+	@Test
+	public void test() throws URISyntaxException {
+		String inputFile = getClass().getResource("test.txt").toURI().toString();
+		logger.info(inputFile);
+		new WordCountTask().run(inputFile);
+	}
+}
